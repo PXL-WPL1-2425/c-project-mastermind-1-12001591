cc563814a9f21bc0abdf53bad9adfb525b6ec6c6
Matthias Sourbron <12001591@student.pxl.be>
2024-11-04 15:12:13 +0100

    MasterMind-02

--- c-project-mastermind-1-12001591/c-project-mastermind-1-12001591/MainWindow.xaml.cs
+++ c-project-mastermind-1-12001591/c-project-mastermind-1-12001591/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -19,6 +20,40 @@ namespace c_project_mastermind_1_12001591
         public MainWindow()
         {
             InitializeComponent();
+           
+            string color = "";
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 4; i++)
+            {
+                Random rnd = new Random();
+                int randomNumber = rnd.Next(1, 7);
+                switch (randomNumber)
+                {
+                    case 1:
+                        color = "Blue";
+                        break;
+                    case 2:
+                        color = "Red";
+                        break;
+                    case 3:
+                        color = "White";
+                        break;
+                    case 4:
+                        color = "Yellow";
+                        break;
+                    case 5:
+                        color = "Orange";
+                        break;
+                    case 6:
+                        color = "Green";
+                        break;
+                }
+                sb.Append(color +",");
+            }
+            Title = "MasterMind (" + sb.ToString() + ")";
+        
         }
+            
     }
+
 }
